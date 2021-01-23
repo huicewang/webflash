74046c6058f8d7b3c8c291409edde4a1145f2607
huicewang <65132090@qq.com>
2021-01-23 10:59:36 +0800

    添加一个测试用例

--- cypress/integration/api/testLogin.js
+++ cypress/integration/api/testLogin.js
@@ -14,4 +14,13 @@ describe('登录接口测试', ()=>{
             body:{username:'huice001',password:'huice001'}})
             .its("body").should('contain',{"code":9999,"msg":"登录时失败"})
     })
+
+
+    it('错误的用户名和密码，提示登录失败', ()=>{
+        cy.request({method:'POST',
+            url:'/prod-api/account/login',
+            form:true,
+            body:{username:'huice002',password:'huice002'}})
+            .its("body").should('contain',{"code":9999,"msg":"登录时失败"})
+    })
 })
